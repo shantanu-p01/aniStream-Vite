dd2a90b4286894d95318fa981ceac7fadc64d981
Shantanu Patil <shantanu.verulkar.01@gmail.com>
2024-09-14 14:02:30 +0530

    22. Added Loader to PlayerPage     - loader will be shown for 1sec when routed from HomePage     - updated Title in index.html     - updated image links in index.html

--- src/Pages/Player/PlayerPage.jsx
+++ src/Pages/Player/PlayerPage.jsx
@@ -22,9 +22,25 @@ const PlayerPage = () => {
   const [likeStatuses, setLikeStatuses] = useState({});
   const [activeSeason, setActiveSeason] = useState(1);
   const [animeName, setAnimeName] = useState('');
+  const [isLoading, setIsLoading] = useState(true); // Loader state
 
   const navigate = useNavigate();
   const location = useLocation();
+
+  // Simulate loading for 1 second when routed
+  useEffect(() => {
+    // Disable scrolling when loading
+    document.body.style.overflow = isLoading ? 'hidden' : 'auto';
+
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Hide loader after 1 second
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer); // Clean up the timer
+      document.body.style.overflow = 'auto'; // Ensure scrolling is re-enabled after unmount
+    };
+  }, [isLoading]);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -131,83 +147,99 @@ const PlayerPage = () => {
   const handleBackClick = () => {
     navigate('/');
   };
-  
-  return (
-    <main className='pt-24 p-2 min-h-fit h-full w-full'>
-      <div className='flex flex-col lg:flex-row items-center lg:items-start gap-2 max-w-7xl mx-auto p-2 bg-black/20 rounded-lg'>
-        {/* Video Player */}
-        <div className='w-full lg:w-2/3'>
-          <div className='w-full pb-2 flex flex-row items-center justify-start gap-2'>
-            <button 
-              className='btn btn-ghost transition duration-300 min-h-fit h-fit p-1 bg-white/20 hover:bg-white/30'
-              onClick={handleBackClick}
-            >
-              <IoIosArrowBack size="24" />
-            </button>
-            <h1 className='text-xl font-semibold'>{animeName} - Episode {currentIndex + 1}</h1>
-          </div>
-          <div className='aspect-w-16 overflow-hidden rounded-lg aspect-h-9'>
-            <Plyr {...plyrProps} />
-          </div>
-          <div className='w-full p-2 flex items-center justify-evenly gap-2 mt-2 rounded-lg bg-black/20'>
-            <button
-              className={`btn btn-ghost h-fit min-h-fit p-1 text-white/80 ${isPreviousDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
-              onClick={handlePreviousEpisode}
-              disabled={isPreviousDisabled}
-            >
-              <IoIosArrowBack size="24" />
-              <h1 className='hidden sm:block pr-2'>Previous</h1>
-            </button>
-            <div className='flex items-center justify-center gap-8'>
-              <button
-                className={`btn btn-ghost h-fit min-h-fit p-1 ${currentLikeStatus === 'like' ? 'text-blue-500' : 'text-white/80'}`}
-                onClick={() => toggleLikeDislike('like')}
-              >
-                <BiSolidLike size="24" />
-                <h1 className='hidden sm:block'>Like</h1>
-              </button>
-              <button
-                className={`btn btn-ghost h-fit min-h-fit p-1 ${currentLikeStatus === 'dislike' ? 'text-red-500' : 'text-white/80'}`}
-                onClick={() => toggleLikeDislike('dislike')}
-              >
-                <BiSolidDislike size="24" />
-                <h1 className='hidden sm:block'>Dislike</h1>
-              </button>
-            </div>
-            <button
-              className={`btn btn-ghost h-fit min-h-fit p-1 text-white/80 ${isNextDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
-              onClick={handleNextEpisode}
-              disabled={isNextDisabled}
-            >
-              <h1 className='hidden sm:block pl-2'>Next</h1>
-              <IoIosArrowForward size="24" />
-            </button>
-          </div>
-        </div>
 
-        {/* Episodes Section */}
-        <div className='w-full lg:w-1/3 rounded-lg bg-black/20'>
-          <h1 className='text-2xl p-2 pb-0 font-semibold'>Season</h1>
-          {/* Season Buttons */}
-          <div className="flex flex-wrap justify-start items-center gap-2 p-2 pb-4">
-            {[1, 2, 3].map((season) => (
-              <Button key={season} isActive={activeSeason === season} onClick={() => handleSeasonChange(season)}>
-                {season}
-              </Button>
-            ))}
-          </div>
-          <h1 className='text-2xl px-2 pb-0 font-semibold'>Episodes</h1>
-          {/* Episode Buttons */}
-          <div className='flex flex-wrap justify-start items-center gap-2 p-2'>
-            {episodes[activeSeason].map(({ episode, file }, index) => (
-              <Button key={index} isActive={episode === activeEpisode} onClick={() => handleEpisodeChange(episode, file)}>
-                {index + 1}
-              </Button>
-            ))}
-          </div>
-        </div>
+  return (
+    <>
+      {isLoading ? ( // Show loader while loading
+      <div className="flex justify-center items-center min-h-svh">
+        <span className="loading loading-dots loading-md"></span>
       </div>
-    </main>
+      ) : (
+      <main className='pt-24 p-2 min-h-fit h-full w-full'>
+          <div className='flex flex-col lg:flex-row items-center lg:items-start gap-2 max-w-7xl mx-auto p-2 bg-black/20 rounded-lg'>
+            {/* Video Player */}
+            <div className='w-full lg:w-2/3'>
+              <div className='w-full pb-2 flex flex-row items-center justify-start gap-2'>
+                <button 
+                  className='btn btn-ghost transition duration-300 min-h-fit h-fit p-1 bg-white/20 hover:bg-white/30'
+                  onClick={handleBackClick}
+                >
+                  <IoIosArrowBack size="24" />
+                </button>
+                <h1 className='text-xl font-semibold'>{animeName} - Episode {currentIndex + 1}</h1>
+              </div>
+              <div className='aspect-w-16 overflow-hidden rounded-lg aspect-h-9'>
+                <Plyr {...plyrProps} />
+              </div>
+              <div className='w-full p-2 flex items-center justify-evenly gap-2 mt-2 rounded-lg bg-black/20'>
+                <button
+                  className={`btn btn-ghost h-fit min-h-fit p-1 text-white/80 ${isPreviousDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+                  onClick={handlePreviousEpisode}
+                  disabled={isPreviousDisabled}
+                >
+                  <IoIosArrowBack size="24" />
+                  <h1 className='hidden sm:block pr-2'>Previous</h1>
+                </button>
+                <div className='flex items-center justify-center gap-8'>
+                  <button
+                    className={`btn btn-ghost h-fit min-h-fit p-1 ${currentLikeStatus === 'like' ? 'text-blue-500' : 'text-white/80'}`}
+                    onClick={() => toggleLikeDislike('like')}
+                  >
+                    <BiSolidLike size="24" />
+                    <h1 className='hidden sm:block'>Like</h1>
+                  </button>
+                  <button
+                    className={`btn btn-ghost h-fit min-h-fit p-1 ${currentLikeStatus === 'dislike' ? 'text-red-500' : 'text-white/80'}`}
+                    onClick={() => toggleLikeDislike('dislike')}
+                  >
+                    <BiSolidDislike size="24" />
+                    <h1 className='hidden sm:block'>Dislike</h1>
+                  </button>
+                </div>
+                <button
+                  className={`btn btn-ghost h-fit min-h-fit p-1 text-white/80 ${isNextDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+                  onClick={handleNextEpisode}
+                  disabled={isNextDisabled}
+                >
+                  <h1 className='hidden sm:block pl-2'>Next</h1>
+                  <IoIosArrowForward size="24" />
+                </button>
+              </div>
+            </div>
+
+            {/* Episodes Section */}
+            <div className='w-full lg:w-1/3 rounded-lg bg-black/20'>
+              <h1 className='text-2xl p-2 pb-0 font-semibold'>Season</h1>
+              {/* Season Buttons */}
+              <div className="flex flex-wrap justify-start items-center gap-2 p-2">
+                {Object.keys(episodes).map((season) => (
+                  <Button
+                    key={season}
+                    isActive={activeSeason === parseInt(season)}
+                    onClick={() => handleSeasonChange(parseInt(season))}
+                  >
+                    {season}
+                  </Button>
+                ))}
+              </div>
+              <h1 className='text-2xl p-2 pb-0 font-semibold'>Episodes</h1>
+              {/* Episode Buttons */}
+              <div className="flex flex-wrap justify-start items-center gap-2 p-2">
+                {currentEpisodes.map((episode) => (
+                  <Button
+                    key={episode.episode}
+                    isActive={activeEpisode === episode.episode}
+                    onClick={() => handleEpisodeChange(episode.episode, episode.file)}
+                  >
+                    {currentEpisodes.findIndex(ep => ep.episode === episode.episode) + 1}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </div>
+      </main>
+      )}
+    </>
   );
 };
 
